3edeff21c8956f773ba45056467ec20a748751c6
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-11-08 22:32:36 +0300

    feat: change pipeline, prepare for multistreaming

--- index.js
+++ index.js
@@ -2,43 +2,53 @@ const fs = require('fs');
 const { stdout, stderr, exit } = process;
 const coder = require('./coder');
 const args = require('./args');
-const checkPrograms = require('./utils/validatePrograms')
+const checkPrograms = require('./utils/validatePrograms');
+const { pipeline } = require('stream');
+const { promisify } = require('util');
+
+const pipelineAsync = promisify(pipeline);
 
 
 const [codeFlag, codeProgram, inputFlag, inputPoint, outputFlag, outputPoint] = args;
 
-const codeProgramsArr = codeProgram.split('-');
-
-const isCorrectProgram = !checkPrograms(codeProgramsArr).includes(false);
+const codeProgramsArr = [ coder ];
+const isCorrectProgram = !checkPrograms(codeProgram.split('-')).includes(false);
 
 if (!isCorrectProgram) {
     stderr.write('Попробуйте ещё раз запустить файл c командами С0, C1, A, R1 или R0  🛑\n');
-    exit();
+    process.exitCode = 1;
 }
 
 if (codeFlag !== '-c') {
     stderr.write('Попробуйте ещё раз запустить файл с флагом -c  🛑\n');
-    exit();
+    process.exitCode = 1;
 }
 
 if (inputFlag !== '-i') {
     stderr.write('Укажите входной файл с флагом -i  🛑\n');
-    exit();
+    process.exitCode = 1;
 }
 
 if (outputFlag !== '-o') {
     stderr.write('Укажите выходной файл с флагом -o  🛑\n');
-    exit();
+    process.exitCode = 1;
 }
 
-let readStream = fs.createReadStream(inputPoint, 'utf-8');
+let readStream = fs.createReadStream(inputPoint);
 let writeStream = fs.createWriteStream(outputPoint);
 
-readStream
-    .pipe(coder)
-    .pipe(writeStream)
-    .on('finish', () => {
-    stdout.write(`Done 🤘👀. Программа кодировки: ${codeProgram}\n`);
-    exit();
+(async function start() {
+    try{
+        await pipelineAsync(
+            readStream,
+            ...codeProgramsArr,
+            writeStream,
+        );
+        stdout.write(`Done 🤘👀. Программа кодировки: ${codeProgram}\n`);
     }
-);
+    catch(err) {
+        stderr.write('pipeline failed with error:');
+    }
+})();
+
+
